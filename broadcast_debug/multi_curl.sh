896b6baa260ee7199d7f6447fd9a6074609fc362
phzhu <phzhu99@gmail.com>
2024-03-18 20:25:28 -0400

    debug file ans script

---
+++ broadcast_debug/multi_curl.sh
@@ -0,0 +1,4 @@
+curl -s 'localhost:26657/broadcast_tx_commit?tx="name=brad"' &
+curl -s 'localhost:26660/broadcast_tx_commit?tx="name=brad"' &
+curl -s 'localhost:26662/broadcast_tx_commit?tx="name=brad"' &
+curl -s 'localhost:26664/broadcast_tx_commit?tx="name=brad"'
