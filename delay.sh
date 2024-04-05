b65d13ec3f4b6dd8700d7356c957dd3eef065f0c
phzhu <phzhu99@gmail.com>
2024-04-04 21:04:42 -0400

    fix delay script

--- delay.sh
+++ delay.sh
@@ -7,5 +7,5 @@ jitters=$3
 # option 1 means adding a default delay for everything
 if [ $option -eq 1 ]; then
     echo "default delay mode, adding delay of ${delay}ms with ${jitters}ms jitters"
-    sudo tc qdisc change dev eth0 root netem delay ${delay}ms ${jitters}ms # 30ms jitters
+    sudo tc qdisc add dev ens3 root netem delay ${delay}ms ${jitters}ms 
 fi
