f2384fbf050d470cbc17c72d44e919063738269a
phzhu <phzhu99@gmail.com>
2024-04-06 01:17:26 -0400

    update delay script with loss

--- delay.sh
+++ delay.sh
@@ -3,7 +3,9 @@
 option=$1
 delay=$2
 jitters=$3
+loss=$4
 
+set -x
 sudo tc qdisc del dev ens3 root
 
 # option 1 means adding a default delay for everything
@@ -11,3 +13,9 @@ if [ $option -eq 1 ]; then
     echo "default delay mode, adding delay of ${delay}ms with ${jitters}ms jitters"
     sudo tc qdisc add dev ens3 root netem delay ${delay}ms ${jitters}ms 
 fi
+
+if [ $option -eq 2 ]; then
+    echo "default delay mode, adding delay of ${delay}ms with ${jitters}ms jitters, and ${loss}% loss"
+    sudo tc qdisc add dev ens3 root netem delay ${delay}ms ${jitters}ms loss ${loss}%
+fi
+
